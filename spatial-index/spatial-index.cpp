82efcf960dad4c5bc46b183356eecb4b9c49b026
Lloyd Wang <LWANG046@e.ntu.edu.sg>
2023-09-03 15:30:11 +0800

    initial commit

---
+++ spatial-index/spatial-index.cpp
@@ -0,0 +1,12 @@
+﻿// spatial-index.cpp : Defines the entry point for the application.
+//
+
+#include "spatial-index.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
